caddffb8ef953ac26d1f18d2611d715fa69f3290
Palaniappan <palaniappan@Palaniappans-MacBook-Air.local>
2018-01-25 11:59:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class Button extends React.Component {
+
+handleClick = () => {
+this.props.onClickFunction(this.props.incrementValue);
+}
+
+render() {
+  return (
+  <button onClick={this.handleClick}>
+  +{this.props.incrementValue}
+  </button>
+  );
+}
+
+}
+
+const Result = (props) => {
+return (<div>{props.counter}</div>)
+}
+
+class App extends React.Component {
+state = {counter:0}
+
+incrementCounter = (incrementValue) => {
+this.setState((prevState)=> ({
+counter:prevState.counter+incrementValue
+}));
+};
+
+render() {
+return(
+<div>
+<Button incrementValue ={1} onClickFunction={this.incrementCounter}/>
+<Button incrementValue ={10} onClickFunction={this.incrementCounter}/>
+<Button incrementValue ={100} onClickFunction={this.incrementCounter}/>
+<Result counter={this.state.counter}/>
+</div>
+);
+}
+}
+
+export default App;
